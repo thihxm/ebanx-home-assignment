f9aaf8f1b7f7a41d7966a37b791ec010162a7308
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2026-02-12 23:08:18 -0300

    feat: implemented event service events handler

---
+++ internal/service/event_service.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/thihxm/ebanx-home-assignment/internal/domain"
+)
+
+type EventService struct {
+	accountService domain.AccountService
+}
+
+func NewEventService(accountService domain.AccountService) *EventService {
+	return &EventService{
+		accountService: accountService,
+	}
+}
+
+func (s *EventService) HandleEvent(event domain.EventRequest) (*domain.EventResponse, error) {
+	switch event.Type {
+	case "deposit":
+		account, err := s.accountService.Deposit(event.Origin, event.Amount)
+		if err != nil {
+			return nil, err
+		}
+		return &domain.EventResponse{
+			Destination: account,
+		}, nil
+	case "withdraw":
+		account, err := s.accountService.Withdraw(event.Origin, event.Amount)
+		if err != nil {
+			return nil, err
+		}
+		return &domain.EventResponse{
+			Origin: account,
+		}, nil
+	case "transfer":
+		originAccount, destinationAccount, err := s.accountService.Transfer(event.Origin, event.Destination, event.Amount)
+		if err != nil {
+			return nil, err
+		}
+		return &domain.EventResponse{
+			Origin:      originAccount,
+			Destination: destinationAccount,
+		}, nil
+	default:
+		return nil, fmt.Errorf("invalid event type: %s", event.Type)
+	}
+}
