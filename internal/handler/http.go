ac0b6949fc2372317ff8467cc513e24481f98098
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2026-02-12 23:32:53 -0300

    feat: implemented http handler with tests

--- internal/handler/http.go
+++ internal/handler/http.go
@@ -1,15 +1,78 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/thihxm/ebanx-home-assignment/internal/domain"
 )
 
 type HTTPHandler struct {
-	accountUseCase domain.AccountUseCase
+	accountService domain.AccountService
+	eventService   domain.EventService
 }
 
-func NewHTTPHandler(accountUseCase domain.AccountUseCase) *HTTPHandler {
+func NewAccountHTTPHandler(accountService domain.AccountService, eventService domain.EventService) *HTTPHandler {
 	return &HTTPHandler{
-		accountUseCase: accountUseCase,
+		accountService: accountService,
+		eventService:   eventService,
 	}
+}
+
+func (h *HTTPHandler) registerRoutes(mux *http.ServeMux) error {
+	mux.HandleFunc("/reset", h.handleReset)
+	mux.HandleFunc("/event", h.handleEvent)
+	mux.HandleFunc("/balance", h.handleGetBalance)
+	return nil
+}
+
+func (h *HTTPHandler) handleReset(w http.ResponseWriter, r *http.Request) {
+	if err := h.accountService.Reset(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *HTTPHandler) handleEvent(w http.ResponseWriter, r *http.Request) {
+	var req domain.EventRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	resp, err := h.eventService.ProcessEvent(req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(resp)
+}
+
+func (h *HTTPHandler) handleGetBalance(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("account_id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	balance, err := h.accountService.GetBalance(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "0")
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%d", balance)
+}
+
+func (h *HTTPHandler) Serve(addr string) error {
+	mux := http.NewServeMux()
+	h.registerRoutes(mux)
+	server := &http.Server{
+		Addr:    addr,
+		Handler: mux,
+	}
+
+	return server.ListenAndServe()
 }
